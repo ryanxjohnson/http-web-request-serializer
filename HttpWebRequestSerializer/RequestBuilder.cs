d032a571a4ecf5264c1daa669167c8c674e60baa
Ryan Johnson <rjohnson@strenuus.com>
2017-08-17 18:14:58 -0500

    Adding files

---
+++ HttpWebRequestSerializer/RequestBuilder.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace HttpWebRequestSerializer
+{
+    public static class RequestBuilder
+    {
+        public static HttpWebRequest BuildBaseHttpWebRequest(this IDictionary<string, object> requestHeaders, string url)
+        {
+            var req = (HttpWebRequest)WebRequest.Create(url);
+
+            foreach (var header in requestHeaders)
+            {
+                var value = (string)header.Value;
+                switch (header.Key)
+                {
+                    case "Method":
+                        req.Method = value;
+                        break;
+                    case "Accept":
+                        req.Accept = value;
+                        break;
+                    case "Connection":
+                        // throws an exception when setting value, use KeepAlive
+                        break;
+                    case "ContentType":
+                        req.ContentType = value;
+                        break;
+                    case "Content-Length":
+                        req.ContentLength = Convert.ToInt64(value);
+                        break;
+                    case "Date":
+                        req.Date = Convert.ToDateTime(value);
+                        break;
+                    case "Expect":
+                        req.Expect = value;
+                        break;
+                    case "Host":
+                        req.Host = value;
+                        break;
+                    case "IfModifiedSince":
+                        req.IfModifiedSince = Convert.ToDateTime(value);
+                        break;
+                    case "KeepAlive":
+                        req.KeepAlive = Convert.ToBoolean(value);
+                        break;
+                    case "Proxy-Connection":
+                        break;
+                    case "Referer":
+                        req.Referer = value;
+                        break;
+                    case "TransferEncoding":
+                        req.TransferEncoding = value;
+                        break;
+                    case "UserAgent":
+                        req.UserAgent = value;
+                        break;
+                    default:
+                        req.Headers[header.Key] = value;
+                        break;
+                }
+            }
+
+            return req;
+        }
+    }
+}
---
+++ HttpWebRequestSerializer/Serializer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Script.Serialization;
+
+namespace HttpWebRequestSerializer
+{
+    public static class Serializer
+    {
+        private static JavaScriptSerializer serializer => new JavaScriptSerializer();
+
+        public static string SerializeRequestProperties(this object properties)
+        {
+            var request = properties.MakeDictionary();
+            return serializer.Serialize(request);
+        }
+
+        public static IDictionary<string, object> DeserializeRequestProperties(this string json)
+        {
+            return serializer.Deserialize<IDictionary<string, object>>(json);
+        }
+
+        public static IDictionary<string, object> MakeDictionary(this object properties)
+        {
+            var dict = properties as IDictionary<string, object>;
+            return dict?.ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
